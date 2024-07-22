323acb3f3968df853858ca32a6f1d452eec4b12b
Otacio Barbosa <otaciowork@gmail.com>
2024-07-22 11:26:10 -0300

    Create page.js

---
+++ src/app/sobre/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function About() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Sobre</h1>
+      </div>
+    </main>
+  );
+}
