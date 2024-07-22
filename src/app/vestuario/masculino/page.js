77fed3f741e47efebea80ba01c58927ced552c58
Otacio Barbosa <otaciowork@gmail.com>
2024-07-22 11:13:28 -0300

    Create page.js

---
+++ src/app/vestuario/masculino/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../../page.module.css";
+
+export default function Masculino() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Masculino</h1>
+      </div>
+    </main>
+  );
+}
